83e85aa77a20303279699db2b8ccc072c19b2896
Cristiana Bolchini <cristiana.bolchini@polimi.it>
2020-10-27 20:34:28 +0100

    Exercises #1 solutions

--- temperature.py
+++ temperature.py
@@ -2,9 +2,10 @@ userX = float(raw_input())
 userY = float(raw_input())
 
 num = int(raw_input())
-pointX = float(raw_input())
-pointY = float(raw_input())
+closestX = float(raw_input())
+closestY = float(raw_input())
 topTemp = float(raw_input())
+closestTemp = topTemp
 minSqDist = (pointX - userX)**2 + \
 			(pointY - userY)**2
 cont = 1
@@ -15,7 +16,14 @@ while cont < num:
 	sqDist = (pointX - userX)**2 + \
 			(pointY - userY)**2
 	if sqDist < minSqDist:
+		closestX = pointX
+		closestY = pointY
+		closestTemp = pointTemp
 		minSqDist = sqDist
 		if pointTemp > topTemp:
 			topTemp = pointTemp
 	cont = cont + 1
+
+print(str(closestX), str(closestY), str(closestTemp))
+if closestTemp == topTemp:
+	print(True)
